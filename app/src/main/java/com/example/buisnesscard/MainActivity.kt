a031e7625a708cb96ffc2d58b5d1a72489c55164
sayyam134 <sayyam134@gmail.com>
2023-12-23 19:22:23 +0530

    Homepage

--- app/src/main/java/com/example/buisnesscard/MainActivity.kt
+++ app/src/main/java/com/example/buisnesscard/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.buisnesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.buisnesscard.ui.theme.BuisnessCardTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +34,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting( modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Sayyam Sancheti",
+            modifier = modifier
+        )
+        Text(
+            text = "sayyamsancheti@icloud.com",
+            modifier = modifier
+        )
+        Text(
+            text = "9179801050",
+            modifier = modifier
+        )
+        Text(
+            text = "Flutter Guy 😜",
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BuisnessCardTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
